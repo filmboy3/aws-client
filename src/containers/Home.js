b61c5c2fce7e5fa7e9b95d604d819187dbe88cfb
filmboy3 <jonathanschwartz30@gmail.com>
2019-02-24 23:54:25 -0500

    completed up to coffee question

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,18 @@
+import takeOnMe from '../audio/takeOnMe.mp3'
 import React, { Component } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { API } from "aws-amplify";
+import shortid from 'shortid';
 import { Link } from "react-router-dom";
 import decor from '../images/BlueDecoration.png';
+import KeyboardEventHandler from 'react-keyboard-event-handler';
 import cityscape from '../images/CityScape.gif';
+import ReactAudioPlayer from 'react-audio-player';
 import office from '../images/MainSetPiece.gif';
+import scottStatic from '../images/ScottGalloway.png';
 import "./Home.css";
-import Iframe from 'react-iframe';
+import { text } from "../text/text.js";
 
 export default class Home extends Component {
   constructor(props) {
@@ -15,7 +20,13 @@ export default class Home extends Component {
 
     this.state = {
       isLoading: true,
-      notes: []
+      notes: [],
+      soundOn: true,
+      textCounter: 0,
+      players: ['Raj', 'Kliment', 'Runtao', 'Bruce', 'Sabbir'],
+      playerName: 'Bob',
+      text: text,
+      masterData: [],
     };
   }
 
@@ -38,33 +49,120 @@ export default class Home extends Component {
     return API.get("notes", "/notes");
   }
 
-  renderNotesList(notes) {
+  newName = (key) => {
+    if (this.state.players[key-1]) {
+      this.setState({
+        playerName: this.state.players[key-1] 
+      })
+      if (this.state.text[this.state.textCounter+1]) {
+        this.setState({
+          textCounter: this.state.textCounter + 1
+        })
+      }
+    }
+  }
+
+  coffee = (key) => {
+    if (key === 'n'){
+      alert(`Jonathan respects your decision and will not ask you out for coffee in real life, ${this.state.playerName}. But for the purpose of the app, would you mind playing along ...?`)
+    }
+    
+    if (this.state.text[this.state.textCounter+1]) {
+      this.setState({
+        textCounter: this.state.textCounter + 1
+      })
+    }
+  }
+
+  renderNotesList(notes, props) {
+    let sound = this.state.soundOn;
+    let lineRender = [];
+    text[this.state.textCounter].forEach(line => {
+        if (line.includes("{scottStatic}")){
+          // let item = line.slice(1, -1);
+          lineRender.push(
+            <img src={scottStatic} alt="scottStatic" height="50%" width="50%" />
+          )
+        } else {
+          lineRender.push(
+            <p key={shortid.generate()}>{line.replace("NAMEPLACEHOLDER", `${this.state.playerName}`)}</p>
+          );
+        }
+      })
+     
     return (
       <div>
-        <img id="flipped_image" src={decor} alt="decoration"/>
-        <h1>WELCOME</h1>
-        <img src={cityscape} id="city_scape" alt="city-scape"/>
-        <img src={office} alt="office"/>
-        <Iframe url="http://www.youtube.com/embed/xDMP3i36naA"
-        width="450px"
-        height="450px"
-        id="myId"
-        className="myClassname"
-        display="initial"
-        position="relative"
-        allowFullScreen/>
-        <img id="flipped_image" src={decor} alt="decoration"/>
+        <img className="flipped_image" src={decor} alt="decoration"/>
+        <h1>THE ONBOARDING TRAIL</h1>
+        {sound && <ReactAudioPlayer
+          src={takeOnMe}
+          autoPlay
+          volume={.1}
+        /> }
+
+        <div class="textBlock">{lineRender}</div>
+        <KeyboardEventHandler
+          handleKeys={['numeric']}
+          onKeyEvent={(key, e) => {
+              // console.log('Numeric triggered');
+              switch (this.state.textCounter) {
+                case 2:
+                  this.newName(key);
+                  break;
+                default:
+                  break;
+              }
+            }
+          }   
+        />
+        <KeyboardEventHandler
+          handleKeys={['y', 'n']}
+          onKeyEvent={(key, e) => {
+              console.log('Yes or No triggered')
+              switch (this.state.textCounter) {
+                case 4:
+                  this.coffee(key);
+                  break;
+                default:
+                  break;
+              }
+            }
+          }
+          />  
+        <KeyboardEventHandler
+          handleKeys={['space']}
+          onKeyEvent={(key, e) => {
+              console.log('Spacebar triggered')
+              if (this.state.text[this.state.textCounter+1]) {
+                this.setState({
+                  textCounter: this.state.textCounter + 1
+                })
+              }
+            }
+          }   
+        />
+        <KeyboardEventHandler
+          handleKeys={['ctrl+s']}
+          onKeyEvent={(key, e) => {
+              console.log('Mute triggered')
+              let soundOn = !this.state.soundOn;
+              this.setState({
+                soundOn
+              })
+            }
+          }   
+        />
+        <img src={decor} alt="decoration"/>
       </div>
     )
 
   }
 
-  renderLander() {
+  renderLander(props) {
     return (
       <div className="lander">
-        <img id="flipped_image" src={decor} alt="decoration"/>
-        <h1>THE ONBOARDING TRAIL</h1>
-        <img src={office} alt="main-office"/>
+        <img className="flipped_image" src={decor} alt="decoration"/>
+        <h1>THE L2 ONBOARDING TRAIL</h1>
         <img src={decor} alt="decoration"/>
         <div>
           <Link to="/signup" className="btn btn-success btn-lg">
@@ -83,7 +181,7 @@ export default class Home extends Component {
   renderNotes() {
     return (
       <div className="notes">
-        <PageHeader>Your Notes</PageHeader>
+        {/* <PageHeader>Your Notes</PageHeader> */}
         <ListGroup>
           {!this.state.isLoading && this.renderNotesList(this.state.notes)}
         </ListGroup>
---
+++ src/text/text.js
@@ -0,0 +1,29 @@
+export const text = [
+  // 0
+  ["It's a journey that will take you across thousands of miles ... of code.  The learning curve is steep, the codebase is wide, and there are countless thorny problems and treacherous bugs littering your path.", 
+  "Will you and your team help navigate Jonathan's safe passage to the L2 frontier, forged by this brave pioneer?", 
+  `{scottStatic}`,
+  "Press SPACEBAR to continue"], 
+  // 1
+  ["To turn sound on/off, press Control-S",
+  "Press SPACEBAR to continue"], 
+  // 2
+  ['There are many incredibly talented engineers on the L2 team.',
+    'You may support Jonathan as:', 
+    '1. Raj, Director of Engineering',
+    '2. Kliment, VP of Engineering',
+    '3. Runtao, Frontend Engineer',
+    '4. Bruce, Frontend Engineer',
+    '5. Sabbir, Backend Engineer',
+    'Please enter the number of your choice: _'],
+  // 3
+  ["Hi, NAMEPLACEHOLDER! Jonathan's almost ready for his first day. Shall we get started?",
+   "Press SPACEBAR to continue"],
+  // 4
+   ["Jonathan has tons of questions about L2's work environment, the role, the team ... but before the journey officially begins: ",
+   "Can he take you out for coffee (or lunch)?",
+   `Press 'y' or 'n'`],
+   // 5
+   ["Please list any/all of your favorite casual eateries in NYC",
+    "Also your preferred orders"]
+];
