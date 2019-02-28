56aca39fbffd1b16fbfa01312d803c6c1c9bd1c8
Jonathan Schwartz <filmboy3@yahoo.com>
2019-02-28 05:13:58 -0500

    Updates

---
+++ precache-manifest.43b293c0ded8df6c4c5c5ae24acdea78.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b0f7dd156dffdcf9cd664a7aef47869",
+    "url": "/aws-client/static/media/Walkman.7b0f7dd1.gif"
+  },
+  {
+    "revision": "52b20227add5568382a4",
+    "url": "/aws-client/static/css/main.9745f147.chunk.css"
+  },
+  {
+    "revision": "2ea51db94833e736feec",
+    "url": "/aws-client/static/js/runtime~main.2ea51db9.js"
+  },
+  {
+    "revision": "81c0aae9bb2eb54bf8fe07e1b68d829e",
+    "url": "/aws-client/static/media/PressStart.81c0aae9.ttf"
+  },
+  {
+    "revision": "682c36f1571850693882",
+    "url": "/aws-client/static/js/2.682c36f1.chunk.js"
+  },
+  {
+    "revision": "967b9f19e274047a79336655fa64d81d",
+    "url": "/aws-client/static/media/BlueDecoration.967b9f19.png"
+  },
+  {
+    "revision": "ee8b1d7778aec6270fd318a84672d4bf",
+    "url": "/aws-client/static/media/RIP.ee8b1d77.png"
+  },
+  {
+    "revision": "1f9d52617c1c1eba0dd3d983254887d9",
+    "url": "/aws-client/static/media/computerLoad.1f9d5261.gif"
+  },
+  {
+    "revision": "d434f9a890f8602144dac08d83ac97a8",
+    "url": "/aws-client/static/media/Medication.d434f9a8.png"
+  },
+  {
+    "revision": "89cd2f70c9e7f5ff443f4ef4e8124c15",
+    "url": "/aws-client/static/media/commute2.89cd2f70.gif"
+  },
+  {
+    "revision": "5bb7a771146e41e79bfc48053b54975f",
+    "url": "/aws-client/static/media/CityScape.5bb7a771.gif"
+  },
+  {
+    "revision": "e451ed9ec7f2ea41bf45a0a6e8a12c38",
+    "url": "/aws-client/static/media/MainSetPiece.e451ed9e.gif"
+  },
+  {
+    "revision": "dc752917a15cdc840c5bb5768fb8acac",
+    "url": "/aws-client/static/media/NYC_ZoomWide_Map.dc752917.png"
+  },
+  {
+    "revision": "8d04ba1fa069c025ddcdba9ee5e803b3",
+    "url": "/aws-client/static/media/ScottGalloway.8d04ba1f.png"
+  },
+  {
+    "revision": "9a7d1ca25c9ea4da3c8bf0a249bba9e5",
+    "url": "/aws-client/static/media/coffee-man.9a7d1ca2.gif"
+  },
+  {
+    "revision": "40031f51ab25db7a3121c0125c592bfd",
+    "url": "/aws-client/static/media/window.40031f51.gif"
+  },
+  {
+    "revision": "507acfc66ee1560e532329314b68a21f",
+    "url": "/aws-client/static/media/ping_pong.507acfc6.gif"
+  },
+  {
+    "revision": "52b20227add5568382a4",
+    "url": "/aws-client/static/js/main.52b20227.chunk.js"
+  },
+  {
+    "revision": "c167cdde171965d82bd75425e3ce940c",
+    "url": "/aws-client/static/media/otherDesk.c167cdde.png"
+  },
+  {
+    "revision": "88cbababb9478890eeab84e6d479e11b",
+    "url": "/aws-client/static/media/DeskHeadphone.88cbabab.gif"
+  },
+  {
+    "revision": "a58d2b82dec9e5d8e6fd7c90a255921d",
+    "url": "/aws-client/static/media/gym.a58d2b82.png"
+  },
+  {
+    "revision": "283d63512ec91cd4001ec62d43d44849",
+    "url": "/aws-client/static/media/scottHeadphone.283d6351.gif"
+  },
+  {
+    "revision": "8e10d921e88f8deefc66f81a822fc16c",
+    "url": "/aws-client/static/media/trophy.8e10d921.gif"
+  },
+  {
+    "revision": "2bc43c1983242fa8aea3d92d38be6eee",
+    "url": "/aws-client/static/media/floppy.2bc43c19.gif"
+  },
+  {
+    "revision": "339c5a23ff4b6679546d43807a563e57",
+    "url": "/aws-client/static/media/programmingSkill.339c5a23.gif"
+  },
+  {
+    "revision": "d591b4cb37080e95003aa68970bfb436",
+    "url": "/aws-client/static/media/BedGif.d591b4cb.gif"
+  },
+  {
+    "revision": "b7f8524e39c2e4a28410873e4d54232a",
+    "url": "/aws-client/static/media/computerWithin.b7f8524e.gif"
+  },
+  {
+    "revision": "b8a155692b424af947ed728e2e18cb4a",
+    "url": "/aws-client/static/media/Milk.b8a15569.png"
+  },
+  {
+    "revision": "d18f9cb0b18abcd490f2b2cb0d18a4b1",
+    "url": "/aws-client/static/media/Buffalo.d18f9cb0.png"
+  },
+  {
+    "revision": "398bf11c4be6a81757e1b8ae3c416f9e",
+    "url": "/aws-client/static/media/CoffeeMan.398bf11c.png"
+  },
+  {
+    "revision": "cea98c362935a63cf4aaff020d02dc54",
+    "url": "/aws-client/static/media/takeOnMe.cea98c36.mp3"
+  },
+  {
+    "revision": "713ad647718093479ef7ba690c320048",
+    "url": "/aws-client/static/media/OldeEnglish.713ad647.ttf"
+  },
+  {
+    "revision": "a2d418630eac24aeff218d734b4e08bc",
+    "url": "/aws-client/static/media/RODEO.a2d41863.ttf"
+  },
+  {
+    "revision": "682c36f1571850693882",
+    "url": "/aws-client/static/css/2.eacd5775.chunk.css"
+  },
+  {
+    "revision": "13a22af8b0272336f4c9b94710cc3f01",
+    "url": "/aws-client/index.html"
+  }
+];
